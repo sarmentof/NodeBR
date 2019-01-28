0ab8e1dcf8fc5749a31fa9e25d4de2a5f39159df
Fabiano Sarmento <fabiano@howto.com.br>
2019-01-28 13:43:56 -0200

    Curso de NodeJS ministrado por Erick Wendel do grupo NodeBR

--- NodeJS/modulo-02/01-lists/for-forin/1.for-forin-forof.js
+++ NodeJS/modulo-02/01-lists/for-forin/1.for-forin-forof.js
@@ -1,3 +1,10 @@
+/* 
+    Exemplos de uso dos tipos de For: For/ForIn/ForOf
+
+    Usamos o console.time e console.timeEnd para marcar o tempo de execução de cada "For"
+
+*/
+
 const service = require('./service')
 
 async function main() {
@@ -5,10 +12,32 @@ async function main() {
         const result = await service.obterPessoas('a')
         const names = []
 
+        // Exemplo usando for
+
+        console.time('for')
         for (let i = 0; i <= result.results.length - 1; i++) {
             const pessoa = result.results[i]
             names.push(pessoa.name)
         }
+        console.timeEnd('for')
+
+        // Exemplo usando forin 
+
+        console.time('forin')
+        for (let i in result.results) {
+            const pessoa = result.results[i]
+            names.push(pessoa.name)
+        }
+        console.timeEnd('forin')
+
+        // Exemplo usando forof
+
+        console.time('forof')
+        for (pessoa of result.results) {
+            names.push(pessoa.name)
+        }
+        console.timeEnd('forof')
+
         console.log(`names`, names)
     } catch (error) {
         console.error(`erro interno`, error)
--- NodeJS/modulo-02/01-lists/for-forin/service.js
+++ NodeJS/modulo-02/01-lists/for-forin/service.js
@@ -1,11 +1,14 @@
+// Serviço de acesso a API Swapi usando Axios
 const axios = require('axios')
 const URL = `https://swapi.co/api/people`
 
-async function obterPessoas (nome) {
+async function obterPessoas(nome) {
     const url = `${URL}/?search=${nome}&format=json`
     const response = await axios.get(url)
     return response.data
 }
+
+// Consulta para testar funcionamento do axios
 
 // obterPessoas('r2')
 //     .then(function (resultado) {
